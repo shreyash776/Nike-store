d478969b2c95e19a33f680aeb423120ca5de061c
shreyash776 <shreyashchaurasia77@gmail.com>
2023-12-31 04:08:29 +0530

    created responsive hero sec

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,19 +9,25 @@ const Header = () => {
      <div className='nav-content'>
      <div className='logo'></div>
      <div className='icon-box'>
-        <div className='search'><i class="fa-solid fa-magnifying-glass fa-lg"></i></div>
-        <div className='like'><i class="fa-regular fa-heart fa-lg"></i></div>
-        <div className='cart'><i class="fa-brands fa-shopify fa-lg"></i></div>
+        <div className='search'><i className="fa-solid fa-magnifying-glass fa-lg"></i></div>
+        <div className='like'><i className="fa-regular fa-heart fa-lg"></i></div>
+        <div className='cart'><i className="fa-brands fa-shopify fa-lg"></i></div>
      </div>
      </div>
      </nav>
-        <div className='heading'>
-            <h1>Play With Electric Nike</h1>
-            <h1>Adapt 2.0 Sneakers</h1>
+      <div className='hero-section'>
+      <div className='heading'>
+            <h1 className='head'>Play With Electric Nike</h1>
+            <h1 className='head'>Adapt 2.0 Sneakers</h1>
+            <button className='explore-btn'>Explore Store</button>
         </div>
 
+       <div className='hero-content'>
+       <div className='videos'></div>
      <div className='shoe'> </div>
-
+      <div className='media-icons'></div>
+       </div>
+      </div>
      </div>
 
 
